cae033abfa4dd03e2c21c8760292215cae99b4a5
pjancura <jancura.paul@gmail.com>
2026-07-14 22:28:02 -0400

    Chapter_05: adds exercise 1

---
+++ chapter_05/examples.ts
@@ -0,0 +1,49 @@
+const message = Date.now() % 2 === 0 ? "Hello Tuesdays!" : null;
+
+const logId = (id: string | number) => {
+    console.log(id);
+}
+
+logId("1234");
+
+logId(1234);
+
+// logId(true);
+
+type Id = string | number;
+
+const logId2 = (id: Id) => {
+    console.log(id);
+}
+
+type AllSortsOfStuff = 
+    string 
+    | number 
+    | boolean
+    | object
+    | null
+    | {
+        id: string;
+        name: string;
+    }
+
+type YesOrNo = "yes" | "no";
+type StatusCode = 200 | 404 | 500;
+
+document.addEventListener(
+  // autocomplete will suggest DOMContentLoaded, mouseover, and so on.
+  "click",
+  () => {},
+);
+
+type DigitalFormat = "MP3" | "FLAC";
+
+type PhysicalFormat = "LP" | "CD" | "Cassette";
+
+type AlbumFormat = DigitalFormat | PhysicalFormat;
+
+const getAlbumFormats  = (format: AlbumFormat) => {
+    // function body
+};
+
+getAlbumFormats("MP3");
